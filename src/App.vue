4837c3a0c3d7888fe7f1cbbd05b72d54fbbdfd5d
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-09-19 11:50:10 +0200

    Completato Template

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,37 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import axios from 'axios';
+import { store } from './store.js';
 
 export default {
   data() {
     return {
-      message: 'Template Vite + Vue'
+
     }
   },
 
   components: {
     AppHeader,
+    AppMain,
   }
 }
 </script>
 
 <template> 
-  <header>
-    <h1> 
-        <AppHeader />
-    </h1>
-  </header>
+  <div>
+    <AppHeader />
+  </div>
+  <div>
+    <AppMain />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use 'assets/scss/main.scss' as *;
+@import "bootstrap/scss/bootstrap";
 
-header {
-    background-color: aquamarine;
-    text-align: center;
-}
 
-h1 {
-    color: lightcoral;
-}
+
 </style>
 
