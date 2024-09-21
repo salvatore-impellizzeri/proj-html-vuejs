2fc2901fcc8eeecf5d89ee3e0a445d542e21914b
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-09-21 18:36:49 +0200

    Aggiunti clienti

--- src/components/SectionTestimonial.vue
+++ src/components/SectionTestimonial.vue
@@ -6,7 +6,29 @@ import { store } from '../store.js';
 export default {
   data() {
     return {
-       store
+       store,
+       clients: [
+        {
+          img:'../../src/assets/img/client-1-1.png',
+          label:'Golden',
+        },
+        {
+          img:'../../src/assets/img/clienty-2.png',
+          label:'Sweety Cafeteria',
+        },
+        {
+          img:'../../src/assets/img/clienty-4.png',
+          label:'Fastlane',
+        },
+        {
+          img:'../../src/assets/img/client-5.png',
+          label:'Mighty Furnitures',
+        },
+        {
+          img:'../../src/assets/img/clienty-3.png',
+          label:'Jack Roller',
+        },
+       ]
     }
   },
 
@@ -18,26 +40,46 @@ export default {
 </script>
 
 <template> 
-
-  <!-- TITLES -->
+  <div class="container-section">
+    <!-- TITLES -->
+    
+    <div>
+      <TitlesSection class="text-center"
+          :red="store.titles[4].red"
+          :bold="store.titles[4].bigger.bold"
+          :medium="store.titles[4].bigger.medium"
+          :smaller="store.titles[4].smaller" />
+    </div>
   
-  <div>
-    <TitlesSection class="text-center"
-        :red="store.titles[4].red"
-        :bold="store.titles[4].bigger.bold"
-        :medium="store.titles[4].bigger.medium"
-        :smaller="store.titles[4].smaller" />
+    <!-- CARDS -->
+  
+    <div class="cards">
+      <TestimonialCards />
+    </div>
+  
+    <!-- CLIENTS -->
+  
+    <div class="clients row m-auto align-items-center justify-content-center gap-2">
+      <div class="col-2 d-flex justify-content-center" v-for="(client, i) in clients" :key="i">
+        <img :src="client.img" :alt="client.label">
+      </div>
+    </div>
   </div>
-
-  <!-- CARDS -->
-
-  <div>
-    <TestimonialCards />
-  </div>
-
 </template>
 
 <style lang="scss" scoped>
+
+.container-section{
+  padding-bottom: 250px;
+}
+
+.clients{
+  width: 80%;
+}
+
+.cards{
+  margin-bottom: 200px;
+}
 
 </style>
 
